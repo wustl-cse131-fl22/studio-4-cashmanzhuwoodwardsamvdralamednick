a9acd6c4cba4e022bfbd5f1db5aa14ccb6343d00
morgan <morgan@172.27.38.75>
2022-10-12 16:43:45 -0500

    Finished

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -19,6 +20,50 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		
+		String shape = in.next();
+		int r = in.nextInt();
+		int g = in.nextInt();
+		int b = in.nextInt();
+		Boolean bool = in.nextBoolean();
+		double w = in.nextDouble();
+		double x = in.nextDouble();
+		double y = in.nextDouble();
+		double z = in.nextDouble();
+
+		Color mycolor = new Color(r, g, b);
+		StdDraw.setPenColor(mycolor);
+		if(shape.equals("ellipse")) {
+			if(bool) {
+				StdDraw.filledEllipse(w, x, y, z);
+				
+			}
+			if(!bool) {
+				StdDraw.ellipse(w, x, y, z);
+
+			}
+			
+		}
+		if(shape.equals("rectangle")) {
+			if(bool) {
+				StdDraw.filledRectangle(w, x, y/2, z/2);
+			}
+			if(!bool) {
+				StdDraw.rectangle(w, x, y/2, z/2);
+
+			}
+			
+		}
+		if(shape.equals("triangle")) {
+			if(bool) {
+				StdDraw.filledRectangle(w, x, y, z);
+
+			}
+			if(!bool) {
+				StdDraw.rectangle(w, x, y/2, z/2);
+			}
+			
+		}
 		
 	}
 }
